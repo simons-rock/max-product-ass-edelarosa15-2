5ab860f62a86f1f5eb17e7b0f6c85dd9641c12b5
Elizabeth de la Rosa <edelarosa15@src-code.simons-rock.edu>
2019-01-29 12:23:53 -0500

    done

--- max_product_fast.c
+++ max_product_fast.c
@@ -1,6 +1,17 @@
 #include "max_product.h"
+#define SWAP(a,b) {int temp; temp=a; a=b; b=temp;}
 
-int max_pairwise_product_fast(int *arr, int size){
-	return 0;
+	int max_pairwise_product_fast(int *arr, int n){
+		int ind=0;
+		for (int i=1; i<n; i++){
+			if(arr[i]>arr[ind]){
+				ind=i;}}
+		SWAP(arr[ind], arr[n-1]);
+		ind=0;
+		for (int i=1; i<n-1; i++){
+			if(arr[i]>arr[ind])
+				ind=i;}
+		SWAP(arr[ind], arr[n-2]);
+		return arr[n-1]*arr[n-2];
 }
 
--- max_product_naive.c
+++ max_product_naive.c
@@ -1,5 +1,13 @@
 #include "max_product.h"
 
 int max_pairwise_product_naive(int * arr, int size){
-	return 0;
+	int a = arr[0], b = arr[1]; 
+	for (int i=0; i<n; i++){
+		for (int j=i+1; j<n; j++){
+			if (arr[i]*arr[j]>a*b){
+				a=arr[i], b=arr[j];
+			}
+		}
+	}
+	return a*b; 
 }
